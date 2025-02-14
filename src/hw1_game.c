e80ab336a563f991d3c7e10fd9d3f4697f1cd993
Diego Pons <diegojean1995@gmail.com>
2025-02-14 23:22:23 +0000

    test

--- src/hw1_game.c
+++ src/hw1_game.c
@@ -8,6 +8,85 @@
 int main(int argc, char **argv) {
     assert(argc == 4); //we want a size, board state, and a list of keys
 	initialize_board(argv[2], argv[3], (int)strtoul(argv[1], NULL, 10));	
-	
+
+    extern int length;
+    extern char board[MAX_LENGTH][MAX_LENGTH];
+    char choice;
+    int piece;
+    int row;
+    int column;
+    bool quit = false;
+    bool valid;
+    while(winning_board() == false || quit == true) {
+        valid = false;
+        current_board();
+        printf("Choose a piece (1-%d) or q to quit: ", length);
+        while(valid == false) {
+            scanf(" %c", &choice);
+            if(choice == 'q') {
+                valid = true;
+                quit = true;
+                break;
+            }
+            else if(choice >= '0' && choice <= '9') {
+                piece = choice - '0';
+                if(piece >= 1 && piece <= length) {
+                    valid = true;
+                    break;
+                }
+            }
+            printf("Invalid choice. Choose a piece (1-%d) or q to quit: ", length);
+        }
+
+        if(quit == true) {
+            break;
+        }
+        valid = false;
+        printf("Choose a row (0-%d) : ", length - 1);
+        while(valid == false) {
+            if (scanf("%d", &row) == 1) {
+                if(row >= 0 && row <= length - 1) {
+                    valid = true;
+                    break;
+                }
+            }
+        printf("Invalid choice. Choose a row (0-%d) : ", length - 1);
+        }
+
+        valid = false;
+        printf("Choose a column (0-%d) : ", length - 1);
+        while(valid == false) {
+            if (scanf("%d", &column) == 1) {
+                if(column >= 0 && column <= length -1) {
+                    valid = true;
+                    break;
+                }
+            }
+        printf("Invalid choice. Choose a column (0-%d) : ", length - 1);
+        }
+
+        if(board[row][column] != '-') {
+            printf("Invalid choice. That space is already occupied.\n");
+            continue;
+        }
+
+        if(duplicate_piece(piece + '0', row, column)) {
+            printf("Invalid choice. There is already a building with that height in that row or column.\n");
+            continue;
+        }
+
+        if(key_violation(piece + '0', row, column)) {
+            printf("Invalid choice. You violate one of the key requirements.\n");
+            continue;
+        }
+            board[row][column] = '0' + piece;
+        }
+
+     if (winning_board() == 1 ) {
+        printf("Congratulations, you have filled the board!\n");
+        current_board();
+    }
+
     return 0;
+    
 }
